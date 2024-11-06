ac1d251a46e0be2f2641b5119467f6023525aa2f
Igor <4430704@gmail.com>
2024-11-06 18:16:18 +0200

    solution with router

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,1 +1,71 @@
-export const Pagination = () => {};
+import cn from 'classnames';
+
+import { PaginationParams } from '../../types';
+import React from 'react';
+
+export const Pagination: React.FC<PaginationParams> = ({
+  total,
+  perPage,
+  currentPage = 1,
+  onPageChange,
+}) => {
+  const numOfPages = Math.ceil(total / perPage);
+
+  return (
+    <>
+      <ul className="pagination">
+        <li
+          className={cn('page-item', {
+            disabled: currentPage === 1,
+          })}
+        >
+          <a
+            data-cy="prevLink"
+            className="page-link"
+            href="#prev"
+            aria-disabled={currentPage === 1}
+            onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
+          >
+            «
+          </a>
+        </li>
+
+        {Array.from({ length: numOfPages }, (_, index) => (
+          <li
+            key={index}
+            className={cn('page-item', {
+              active: index + 1 === currentPage,
+            })}
+          >
+            <a
+              data-cy="pageLink"
+              className="page-link"
+              href="#"
+              onClick={() => onPageChange(index + 1)}
+            >
+              {index + 1}
+            </a>
+          </li>
+        ))}
+
+        <li
+          className={cn('page-item', {
+            disabled: currentPage === numOfPages,
+          })}
+        >
+          <a
+            data-cy="nextLink"
+            className="page-link"
+            href="#"
+            aria-disabled={currentPage === numOfPages}
+            onClick={() =>
+              currentPage < numOfPages && onPageChange(currentPage + 1)
+            }
+          >
+            »
+          </a>
+        </li>
+      </ul>
+    </>
+  );
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import { createRoot } from 'react-dom/client';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { App } from './App';
+import React from 'react';
 
-createRoot(document.getElementById('root') as HTMLElement).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
